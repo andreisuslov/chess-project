0904684044dcef5a264818b3538b919db6393ff5
andreisuslov <truvord@gmail.com>
2025-11-08 14:11:39 -0400

    Fix position validation with board size parameter

--- chess_piece.py
+++ chess_piece.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 class Chess_Piece(ABC):
     def __init__(self, ID, initial_position, color, direction, board_size=(8, 8)):
         self._ID = ID
-        self._position = initial_position if self._is_valid_position(initial_position) else (None, None)
+        self._position = initial_position if self._is_valid_position(initial_position, board_size) else (None, None)
         self._color = color
         self._direction = direction
         self._board_size = board_size
